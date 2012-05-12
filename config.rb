2e266f53c503e89a2da006cc8eaff28c09cc51ac
Michael Parenteau <prmparenteau@aol.com>
2012-05-12 07:20:07 -0400

    made a toy website about a compass ribbon mixin

---
+++ config.rb
@@ -0,0 +1,17 @@
+### 
+# Compass
+###
+compass_config do |config|
+  config.output_style = :compact
+end
+
+configure :build do
+  # For example, change the Compass output style for deployment
+  activate :minify_css
+
+  # Compress PNGs after build
+  # First: gem install middleman-smusher
+  require "middleman-smusher"
+  activate :smusher
+  
+end
